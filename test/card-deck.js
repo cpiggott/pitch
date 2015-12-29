f52a8c6f320d5db08069ff9071196fe441bba32e
Eric Dorsey <eric@ericdorsey.com>
2015-12-29 01:16:43 -0600

    more tests

--- test/card-deck.js
+++ test/card-deck.js
@@ -8,4 +8,47 @@ describe("Card deck", function() {
       return assert.equal(deck.length, 54);
     });
   });
-})
+  
+  describe("#isSameSuit", function() {
+    describe("Has same suit", function() {
+      let isSameSuit = cardDeck.isSameSuit({ suit: "CLUB" }, { suit: "CLUB" });
+      it("should have same suit", function() {
+        return assert(isSameSuit);
+      });
+    });
+    describe("Has different suit", function() {
+      let isSameSuit = cardDeck.isSameSuit({ suit: "CLUB" }, { suit: "SPADE" });
+      it("should have same suit", function() {
+        return assert(!isSameSuit);
+      });
+    });
+    describe("Has different suit (joker)", function() {
+      let isSameSuit = cardDeck.isSameSuit({ suit: "CLUB" }, { suit: "JOKER" });
+      it("should have same suit", function() {
+        return assert(!isSameSuit);
+      });
+    });
+  });
+  
+  describe("#isOffSuit", function(suit, card) {
+    describe("Is an offsuit", function() {
+      let isOffSuit = cardDeck.isOffSuit({ suit: "CLUB" }, { suit: "SPADE" });
+      it("should be an offsuit", function() {
+        assert(isOffSuit);
+      });
+    });
+    describe("Is not an offsuit", function() {
+      let isOffSuit = cardDeck.isOffSuit({ suit: "CLUB" }, { suit: "HEART" });
+      it("should not be an offsuit", function() {
+        assert(!isOffSuit);
+      });
+    });
+    describe("Is not an offsuit (joker)", function() {
+      let isOffSuit = cardDeck.isOffSuit({ suit: "CLUB" }, { suit: "JOKER" });
+      it("should not be an offsuit", function() {
+        assert(!isOffSuit);
+      });
+    });
+  });
+  
+});
