d32f971b538b089645482db54768f111d281ef21
Eric Dorsey <eric@ericdorsey.com>
2015-12-30 03:02:34 -0600

    new game

--- server/reducer/index.js
+++ server/reducer/index.js
@@ -1,3 +1,13 @@
-export default function reducer(state = {}, action) {
+import * as cardDeck from '../services/card-deck'
+import _ from 'lodash'
+import uniqId from 'uniq-id'
+
+export default function reducer(state, action) {
+  if (action.type === "NEW_GAME") {
+    state = _.assign({}, state);
+    state.games[uniqId()] = {
+      deck: cardDeck.createDeck()
+    }
+  }
   return state;
 }
--- server/services/store.js
+++ server/services/store.js
@@ -3,11 +3,15 @@ import reducer from '../reducer';
 import thunkMiddleware from 'redux-thunk';
 import createLoggerMiddleware from 'redux-node-logger';
 
+const INITIAL_STATE = {
+  games: {}
+};
+
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware,
   createLoggerMiddleware()
 )(createStore);
 
-export default function makeStore() {
-  return createStoreWithMiddleware(reducer);
+export default function makeStore(initialState = INITIAL_STATE) {
+  return createStoreWithMiddleware(reducer, initialState);
 }
